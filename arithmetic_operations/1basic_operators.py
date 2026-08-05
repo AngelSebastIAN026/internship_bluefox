7dfacca604ba9f34729513696ea8b6350823adc8
ANGEL SEBASTIAN <angelsebastianpo3@example.com>
2026-08-05 18:05:00 +0530

    operations

--- arithmetic_operations/1basic_operators.py
+++ arithmetic_operations/1basic_operators.py
@@ -1,28 +1,30 @@
 def sum_of_two_numbers(num1, num2):
     return num1 + num2
 
+def sub_of_two_numbers(num1, num2):
+    return num1 - num2
 
 def mul_of_two_numbers(num1, num2):
     return num1 * num2
 
-
-def sub_of_two_numbers(num1, num2):
-    return num1 - num2
-
-
 def div_of_two_numbers(num1, num2):
     return num1 / num2
 
+def floor_div_of_two_numbers(num1, num2):
+    return num1 // num2
+
+def remain_of_two_numbers(num1, num2):
+    return num1 % num2
 
 def main():
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
-    print(f"Sum: {sum_of_two_numbers(num1, num2)}")
-    print(f"Product: {mul_of_two_numbers(num1, num2)}")
-    print(f"Difference: {sub_of_two_numbers(num1, num2)}")
-    print(f"Quotient: {div_of_two_numbers(num1, num2)}")
+
+    print(f"Sum of {num1} + {num2} = {sum_of_two_numbers(num1, num2)}")
+    print(f"Product of {num1} * {num2} = {mul_of_two_numbers(num1, num2)}")
+    print(f"dividion of {num1} / {num2} = {div_of_two_numbers(num1, num2)}")
+    print(f"Floor of {num1} // {num2} = {floor_div_of_two_numbers(num1, num2)}")
+    print(f"modulus of {num1} % {num2 } = {remain_of_two_numbers(num1, num2)}")
 
 
-if __name__ == "__main__":
-    main()
-
+main()
