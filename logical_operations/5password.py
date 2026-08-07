2fa445c13d8601df9e51446c401f2dcf047d76e1
ANGEL SEBASTIAN <angelsebastianpo3@example.com>
2026-08-07 17:41:22 +0530

    odd or even

--- logical_operations/5password.py
+++ logical_operations/5password.py
@@ -2,12 +2,10 @@ def main() :
     password = str(input("Enter the password: "))
     
     if len(password) <= 8:
-       print("password is incorrect")
+       print("password is incorrect, must have 8 characters")
     if not any(char.isdigit() for char in password):
         print("password must have a digit")
     if not any(char.isupper() for char in password):
         print("password must have a uppercase")
-    else:
-        print("correct password")
         
 main()
