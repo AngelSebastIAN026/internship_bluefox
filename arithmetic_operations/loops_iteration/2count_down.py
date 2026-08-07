69130ee82f7494c9b149c67c940060cec5ac891e
ANGEL SEBASTIAN <angelsebastianpo3@example.com>
2026-08-07 23:54:40 +0530

    n times printing

--- arithmetic_operations/loops_iteration/2count_down.py
+++ arithmetic_operations/loops_iteration/2count_down.py
@@ -1,8 +1,9 @@
-import time
+
 
 def main() :
     num = int(input("Enter the number: "))
     
+    import time
     for i in range(num, -1, -1):
         print(i)
         time.sleep(1)
---
+++ arithmetic_operations/loops_iteration/3n_times.py
@@ -0,0 +1,9 @@
+def main() :
+    num = int(input("Enter the number: "))
+    
+    for i in range (1, num+1):
+        for j in range(i):
+           print(i, end=" ")
+           
+           
+main()
