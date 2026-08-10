215f944b5a3d58147bbd91995691ad4da1675ffb
ANGEL SEBASTIAN <angelsebastianpo3@example.com>
2026-08-10 17:08:20 +0530

    add multiply + 1

---
+++ loops_iteration/5positive_evenadd.py
@@ -0,0 +1,13 @@
+def main() :
+    num = int(input("Enter the number: "))
+    
+        
+    if num % 2 == 0:
+        print("the number is even")
+        print(num // 2)
+            
+    else:
+        print("The nuber is odd")
+        print(num * 3 + 1) 
+           
+main()
