ab09e7a45a8e9e461ff31fa16e9222d6adbf7083
ANGEL SEBASTIAN <angelsebastianpo3@example.com>
2026-08-17 18:44:30 +0530

    calculator from 1-5

--- menu_driven/1calculator.py
+++ menu_driven/1calculator.py
@@ -29,17 +29,29 @@ def main() :
     while True:
         num1 = int(input("Enter the first number: "))
         num2 = int(input("Enter the second number: "))
+        
+        if num1 < 1 or num1 > 5 or num2 < 1 or num2 > 5:
+            print("Invalid choice, only enter number between 1-5")
+            continue
+            
         choice = int(input(select_option_message))
         match choice:
             case 1:
                 print(f"Sum of {num1} and {num2} is {sum_of_two(num1, num2)}")
+
             case 2:
                 print(f"Mul of {num1} and {num2} is {mul_of_two(num1, num2)}")
+
             case 3:
                 print(f"Div of {num1} and {num2} is {div_of_two(num1, num2)}")
+
             case 4:
-                print(f"diff of {num1} and {num2} is {diff_of_two(num1, num2)}")
-            case _:
+                print(f"Diff of {num1} and {num2} is {diff_of_two(num1, num2)}")
+
+            case 5:
                 print("Exiting...")
-                exit()
+                break
+
+            case _:
+                print("Invalid choice. Please select 1-5.")
 main()
